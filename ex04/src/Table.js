cb41c0acf1af16a9355c7adc983af446241ba571
opajkovic <oljaolja170@gmail.com>
2022-05-20 18:09:30 +0200

    added

---
+++ ex04/src/Table.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Table = (props) => {
+    const { characterData, removeCharacter } = props;
+
+    const TableHeader = () => {
+        return (
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                </tr>
+            </thead>
+        )
+    }
+    const TableBody = (props) => {
+        const rows = props.characterData.map((row, index) => {
+            return (
+                <tr key={index}>
+                    <td>{row.firstName}</td>
+                    <td>{row.lastName}</td>
+                    <td>
+                        <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                    </td>
+                </tr>
+            );
+        });
+        return <tbody>{rows}</tbody>
+
+    }
+    return (
+        <table>
+            <TableHeader />
+            <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+        </table>
+    )
+}
+
+
+
+export default Table; 
---
+++ ex05/src/SimpleForm.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class SimpleForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: "Yoda"};
+    }
+
+    onChangeHandler = (event) => {
+        this.setState({name: event.target.value});
+    }
+    render() {
+        return (
+            <form>
+                <h1>Name: {this.state.name}</h1>
+                <p>First Name:</p>
+                <input type='text' onChange={this.onChangeHandler}/>
+            </form>
+        )
+    }
+}
+
+export default SimpleForm;
