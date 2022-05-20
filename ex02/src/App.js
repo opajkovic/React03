0f2251168866e3dcfefb1bf51ec4998b2c6f7140
opajkovic <oljaolja170@gmail.com>
2022-05-20 14:13:09 +0200

    added

---
+++ ex02/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Plant from './Plant';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Plant />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ ex02/src/Plant.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Plant extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { type: "Flowering Plant"};
+    }
+    render() {
+        return (
+            <h2>Plant type is {this.state.type}</h2>
+        )
+    }
+}
+
+export default Plant;
